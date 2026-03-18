99ae4c072099bd6ac4397f78d88d885ab9a446d9
henry2craftman <henry2craftman@gmail.com>
2026-03-18 14:27:49 +0900

    feat: UIManager 추가 연동

---
+++ Assets/Scripts/MPS/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MPS 
+{ 
+    public class UIManager : MonoBehaviour
+    {
+        public TowerLamp towerLamp;
+        public void OnCylinderBtnClkEvent(Cylinder cyl)
+        {
+            if(cyl.solenoidType == Cylinder.SolenoidType.단동형)
+            {
+                cyl.frontSignal_SOL = !cyl.frontSignal_SOL;
+            }
+            else
+            {
+                cyl.frontSignal_SOL = !cyl.frontSignal_SOL;
+                cyl.backSignal_SOL = !cyl.backSignal_SOL;
+            }
+        }
+
+        public void OnLampBtnClkEvent(string color)
+        {
+            switch(color)
+            {
+                case "red":
+                    towerLamp.redLampSignal = !towerLamp.redLampSignal;
+                    break;
+                case "yellow":
+                    towerLamp.yellowLampSignal = !towerLamp.yellowLampSignal;
+                    break;
+                case "green":
+                    towerLamp.greenLampSignal = !towerLamp.greenLampSignal;
+                    break;
+            }
+        }
+
+        public void OnCWBtnClkEvent(Conveyor conv)
+        {
+            conv.cWSignal = !conv.cWSignal;
+        }
+
+        public void OnCCWBtnClkEvent(Conveyor conv)
+        {
+            conv.cCWSignal = !conv.cCWSignal;
+        }
+
+        public void OnLoaderBtnClkEvent(Loader loader)
+        {
+            loader.loadSignal = !loader.loadSignal;
+        }
+    }
+}
